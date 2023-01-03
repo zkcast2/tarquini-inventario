7787965e6ca8f03060c869f76d694c30bdd27012
zkcast2 <ezekqiel@gmail.com>
2023-01-03 16:07:12 -0300

    asd

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 const { conn } = require("./src/db.js");
 const server = require("./src/app.js");
-const axios = require('axios')
 
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`Taquini's inventario // listening at ${process.env.PORT}`);
-    axios.post(`${process.env.URLBASE}:${process.env.PORT}/fulldb`);
   });
 });
