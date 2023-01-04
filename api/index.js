21a9331b330673d9d7b8d81426c16cfd80765440
zkcast2 <ezekqiel@gmail.com>
2023-01-04 20:43:25 -0300

    a

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const { conn } = require("./src/db.js");
 const server = require("./src/app.js");
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`Taquini's inventario // listening at ${process.env.PORT}`);
   });
